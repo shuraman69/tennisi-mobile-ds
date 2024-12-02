ed6da9c535d0b8047036029783319b8f18750bc7
shuraman <a.fateev@stayfitt.ru>
2024-12-02 22:01:10 +0300

    feat: new release 1.0.0

--- scripts/check-branch.sh
+++ scripts/check-branch.sh
@@ -2,7 +2,7 @@
 CURRENT_BRANCH=$(git branch --show-current)
 
 # Проверка, является ли текущая ветка main
-if [ "$CURRENT_BRANCH" != "main" ]; then
+if [ "$CURRENT_BRANCH" == "main" ]; then
   echo "Вы сейчас на другой ветке: $CURRENT_BRANCH"
   exit 1  # Завершаем выполнение скрипта с кодом ошибки
 else
