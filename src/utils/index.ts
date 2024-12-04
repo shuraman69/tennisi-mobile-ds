297175e483dba2e0fa2584a325773d9f8d63604a
shuraman <a.fateev@stayfitt.ru>
2024-12-04 11:30:45 +0300

    feat(mds): add HapticsService.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './getRestyleProps';
 export * from './isEqualJson';
 export { default as ToastService } from './ToastService';
+export { default as HapticsService } from './HapticsService';
